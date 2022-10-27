18d8e9bbb41965e4f3d2ffcc8e0fba4785cdc7da
Ian Castillo <donelianc@icloud.com>
2022-10-28 00:21:37 +0200

    New features added

--- competitions/tabular-playground-series-oct-2022/src/models/train_model.py
+++ competitions/tabular-playground-series-oct-2022/src/models/train_model.py
@@ -45,10 +45,15 @@ class BaseModel:
         ), "Model has not been configured yet. Run read_config() first."
 
         self.data = read_feather("../" + self.config["paths"]["data"])
+        
+        dtypes_df = read_csv("../" + self.config["paths"]["train_dtypes"])
+        dtypes = {k: v for (k, v) in zip(dtypes_df.column, dtypes_df.dtype)}
+        self.data = self.data.astype(dtypes)
+        
         self.data.set_index(self.config["data"]["index"], inplace=True)
 
-        model_features = read_csv("../" + self.config["paths"]["dtypes"])
 
+        model_features = read_csv("../" + self.config["paths"]["test_dtypes"])
         self.features_in = [
             feature for feature in model_features.column.tolist() if feature != "id"
         ]
@@ -64,203 +69,241 @@ class BaseModel:
             self.config is not None
         ), "Model has not been configured yet. Run read_config() first."
 
+        transformer_list = []
+
         player_pipe_meta = dict()
-        for feature in self.config["model"]["features"]["player"]:
+        if self.config["model"]["features"]["player"] is not None:
+            for feature in self.config["model"]["features"]["player"]:
 
-            if feature == "demolished":
-                player_pipe_meta[feature] = {
-                    "transformer": PlayerDemolished(),
-                    "columns": selector("p[0-5]"),
-                }
+                if feature == "demolished":
+                    player_pipe_meta[feature] = {
+                        "transformer": PlayerDemolished(),
+                        "columns": selector("p[0-5]"),
+                    }
 
-            if feature == "velocity":
-                player_velocity_pipe = Pipeline(
-                    steps=[
-                        ("imputer", SimpleImputer(strategy="constant", fill_value=0.0)),
-                        ("scaler", MinMaxScaler()),
-                    ]
-                )
-                player_pipe_meta[feature] = {
-                    "transformer": player_velocity_pipe,
-                    "columns": selector("p[0-5]_vel_"),
-                }
+                if feature == "velocity":
+                    player_velocity_pipe = Pipeline(
+                        steps=[
+                            ("imputer", SimpleImputer(strategy="constant", fill_value=0.0)),
+                            ("scaler", MinMaxScaler()),
+                        ]
+                    )
+                    player_pipe_meta[feature] = {
+                        "transformer": player_velocity_pipe,
+                        "columns": selector("p[0-5]_vel_"),
+                    }
 
-            if feature == "speed":
-                player_speed_pipe = Pipeline(
-                    steps=[
-                        ("speed", PlayerSpeed()),
-                        ("imputer", SimpleImputer(strategy="constant", fill_value=0.0)),
-                        ("scaler", StandardScaler()),
-                    ]
-                )
-                player_pipe_meta[feature] = {
-                    "transformer": player_speed_pipe,
-                    "columns": selector("p[0-5]_vel_]"),
-                }
+                if feature == "speed":
+                    player_speed_pipe = Pipeline(
+                        steps=[
+                            ("speed", PlayerSpeed()),
+                            ("imputer", SimpleImputer(strategy="constant", fill_value=0.0)),
+                            ("scaler", StandardScaler()),
+                        ]
+                    )
+                    player_pipe_meta[feature] = {
+                        "transformer": player_speed_pipe,
+                        "columns": selector("p[0-5]_vel_"),
+                    }
 
-            if feature == "position":
-                player_position_pipe = Pipeline(
-                    steps=[
-                        ("imputer", SimpleImputer(strategy="constant", fill_value=0.0)),
-                        ("scaler", MinMaxScaler()),
-                    ]
-                )
-                player_pipe_meta[feature] = {
-                    "transformer": player_position_pipe,
-                    "columns": selector("p[0-5]_pos_"),
-                }
+                if feature == "position":
+                    player_position_pipe = Pipeline(
+                        steps=[
+                            ("imputer", SimpleImputer(strategy="constant", fill_value=0.0)),
+                            ("scaler", StandardScaler()),
+                        ]
+                    )
+                    player_pipe_meta[feature] = {
+                        "transformer": player_position_pipe,
+                        "columns": selector("p[0-5]_pos_"),
+                    }
 
-        player_trans = ColumnTransformer(
-            transformers=[
-                (name, pipe["transformer"], pipe["columns"])
-                for name, pipe in player_pipe_meta.items()
-            ],
-        )
+                if feature == "distance_to_team":
+                    player_distance_to_team_pipe = Pipeline(
+                        steps=[
+                            ("distance_team", PlayerDistanceToTeam()),
+                            ("scaler", MaxAbsScaler()),
+                        ]
+                    )
+                    player_pipe_meta[feature] = {
+                        "transformer": player_distance_to_team_pipe,
+                        "columns": selector("p[0-5]_pos_"),
+                    }
+
+                if feature == "distance_to_orbs":
+                    player_distance_to_orbs_pipe = Pipeline(
+                        steps=[
+                            ("distance_orbs", PlayerDistanceToOrbs()),
+                            ("scaler", MaxAbsScaler()),
+                        ]
+                    )
+                    player_pipe_meta[feature] = {
+                        "transformer": player_distance_to_orbs_pipe,
+                        "columns": selector("p[0-5]_pos_"),
+                    }
+
+        if len(player_pipe_meta) > 0:
+            player_trans = ColumnTransformer(
+                transformers=[
+                    (name, pipe["transformer"], pipe["columns"])
+                    for name, pipe in player_pipe_meta.items()
+                ],
+            )
+            transformer_list.append(("player", player_trans))
 
         ball_pipe_meta = dict()
-        for feature in self.config["model"]["features"]["ball"]:
+        if self.config["model"]["features"]["ball"] is not None:
+            for feature in self.config["model"]["features"]["ball"]:
 
-            if feature == "velocity":
-                ball_velocity_pipe = Pipeline(
-                    steps=[
-                        ("imputer", SimpleImputer(strategy="constant", fill_value=0.0)),
-                        ("scaler", MaxAbsScaler()),
-                    ]
-                )
-                ball_pipe_meta[feature] = {
-                    "transformer": ball_velocity_pipe,
-                    "columns": selector("ball_vel_"),
-                }
+                if feature == "velocity":
+                    ball_velocity_pipe = Pipeline(
+                        steps=[
+                            ("imputer", SimpleImputer(strategy="constant", fill_value=0.0)),
+                            ("scaler", MinMaxScaler()),
+                        ]
+                    )
+                    ball_pipe_meta[feature] = {
+                        "transformer": ball_velocity_pipe,
+                        "columns": selector("ball_vel_"),
+                    }
 
-            if feature == "speed":
-                ball_speed_pipe = Pipeline(
-                    steps=[
-                        ("speed", BallSpeed()),
-                        ("scaler", StandardScaler()),
-                    ]
-                )
-                ball_pipe_meta[feature] = {
-                    "transformer": ball_speed_pipe,
-                    "columns": selector("ball_vel_]"),
-                }
+                if feature == "speed":
+                    ball_speed_pipe = Pipeline(
+                        steps=[
+                            ("speed", BallSpeed()),
+                            ("scaler", StandardScaler()),
+                        ]
+                    )
+                    ball_pipe_meta[feature] = {
+                        "transformer": ball_speed_pipe,
+                        "columns": selector("ball_vel_"),
+                    }
 
-            if feature == "position":
-                ball_position_pipe = Pipeline(
-                    steps=[
-                        ("imputer", SimpleImputer(strategy="constant", fill_value=0.0)),
-                        ("scaler", MinMaxScaler()),
-                    ]
-                )
-                ball_pipe_meta[feature] = {
-                    "transformer": ball_position_pipe,
-                    "columns": selector("ball_pos_"),
-                }
+                if feature == "position":
+                    ball_position_pipe = Pipeline(
+                        steps=[
+                            ("imputer", SimpleImputer(strategy="constant", fill_value=0.0)),
+                            ("scaler", StandardScaler()),
+                        ]
+                    )
+                    ball_pipe_meta[feature] = {
+                        "transformer": ball_position_pipe,
+                        "columns": selector("ball_pos_"),
+                    }
 
-            if feature == "distance_to_goal_posts":
-                distance_to_goal_posts_pipe = Pipeline(
-                    steps=[
-                        (
-                            "distance",
-                            DistanceBallGoalPosts(
-                                posts=[
-                                    self.config["data"]["goal_post1"],
-                                    self.config["data"]["goal_post2"],
-                                ]
+                if feature == "distance_to_goal_posts":
+                    distance_to_goal_posts_pipe = Pipeline(
+                        steps=[
+                            (
+                                "distance",
+                                DistanceBallGoalPosts(
+                                    posts=[
+                                        self.config["data"]["goal_post1"],
+                                        self.config["data"]["goal_post2"],
+                                    ]
+                                ),
                             ),
-                        ),
-                        ("scaler", MinMaxScaler()),
-                    ]
-                )
-                ball_pipe_meta[feature] = {
-                    "transformer": distance_to_goal_posts_pipe,
-                    "columns": selector("ball_pos_"),
-                }
+                            ("scaler", MaxAbsScaler()),
+                        ]
+                    )
+                    ball_pipe_meta[feature] = {
+                        "transformer": distance_to_goal_posts_pipe,
+                        "columns": selector("ball_pos_"),
+                    }
 
-        ball_trans = ColumnTransformer(
-            transformers=[
-                (name, pipe["transformer"], pipe["columns"])
-                for name, pipe in ball_pipe_meta.items()
-            ],
-        )
+        if len(ball_pipe_meta) > 0:
+            ball_trans = ColumnTransformer(
+                transformers=[
+                    (name, pipe["transformer"], pipe["columns"])
+                    for name, pipe in ball_pipe_meta.items()
+                ],
+            )
+            transformer_list.append(("ball", ball_trans))
 
         timer_pipe_meta = dict()
-        for feature in self.config["model"]["features"]["timer"]:
+        if self.config["model"]["features"]["timer"] is not None:
+            for feature in self.config["model"]["features"]["timer"]:
 
-            if feature == "timer":
-                timer_pipe = Pipeline(
-                    steps=[
-                        (
-                            "timer",
-                            FunctionTransformer(abs, feature_names_out="one-to-one"),
-                        ),
-                        ("scaler", MaxAbsScaler()),
-                    ]
-                )
-                timer_pipe_meta[feature] = {
-                    "transformer": timer_pipe,
-                    "columns": selector("boost[0-5]_timer"),
-                }
+                if feature == "timer":
+                    timer_pipe = Pipeline(
+                        steps=[
+                            (
+                                "timer",
+                                FunctionTransformer(abs, feature_names_out="one-to-one"),
+                            ),
+                            ("scaler", MaxAbsScaler()),
+                        ]
+                    )
+                    timer_pipe_meta[feature] = {
+                        "transformer": timer_pipe,
+                        "columns": selector("boost[0-5]_timer"),
+                    }
 
-        timer_trans = ColumnTransformer(
-            transformers=[
-                (name, pipe["transformer"], pipe["columns"])
-                for name, pipe in timer_pipe_meta.items()
-            ],
-        )
+        if len(timer_pipe_meta) > 0:
+            timer_trans = ColumnTransformer(
+                transformers=[
+                    (name, pipe["transformer"], pipe["columns"])
+                    for name, pipe in timer_pipe_meta.items()
+                ],
+            )
+            transformer_list.append(("timer", timer_trans))
 
         booster_pipe_meta = dict()
-        for feature in self.config["model"]["features"]["booster"]:
+        if self.config["model"]["features"]["booster"] is not None:
+            for feature in self.config["model"]["features"]["booster"]:
 
-            if feature == "booster":
-                booster_pipe = Pipeline(
-                    steps=[
-                        ("imputer", SimpleImputer(strategy="constant", fill_value=0.0)),
-                        ("scaler", MaxAbsScaler()),
-                    ]
-                )
-                booster_pipe_meta[feature] = {
-                    "transformer": booster_pipe,
-                    "columns": selector("p[0-5]_boost"),
-                }
+                if feature == "booster":
+                    booster_pipe = Pipeline(
+                        steps=[
+                            ("imputer", SimpleImputer(strategy="constant", fill_value=0.0)),
+                            ("scaler", MaxAbsScaler()),
+                        ]
+                    )
+                    booster_pipe_meta[feature] = {
+                        "transformer": booster_pipe,
+                        "columns": selector("p[0-5]_boost"),
+                    }
 
-        booster_trans = ColumnTransformer(
-            transformers=[
-                (name, pipe["transformer"], pipe["columns"])
-                for name, pipe in booster_pipe_meta.items()
-            ],
-        )
+        if len(booster_pipe_meta) > 0:
+            booster_trans = ColumnTransformer(
+                transformers=[
+                    (name, pipe["transformer"], pipe["columns"])
+                    for name, pipe in booster_pipe_meta.items()
+                ],
+            )
+            transformer_list.append(("booster", booster_trans))
 
         team_pipe_meta = dict()
-        for feature in self.config["model"]["features"]["team"]:
+        if self.config["model"]["features"]["team"] is not None:
+            for feature in self.config["model"]["features"]["team"]:
 
-            if feature == "centroid":
-                team_pipe = Pipeline(
-                    steps=[
-                        ("team_centroid", TeamCentroid()),
-                        ("scaler", StandardScaler()),
-                    ]
-                )
-                team_pipe_meta[feature] = {
-                    "transformer": team_pipe,
-                    "columns": selector("p[0-5]_pos_"),
-                }
+                if feature == "centroid":
+                    team_pipe = Pipeline(
+                        steps=[
+                            ("team_centroid", TeamCentroid()),
+                            ("scaler", StandardScaler()),
+                        ]
+                    )
+                    team_pipe_meta[feature] = {
+                        "transformer": team_pipe,
+                        "columns": selector("p[0-5]_pos_"),
+                    }
 
-        team_trans = ColumnTransformer(
-            transformers=[
-                (name, pipe["transformer"], pipe["columns"])
-                for name, pipe in team_pipe_meta.items()
-            ],
-        )
+        if len(team_pipe_meta) > 0:
+            team_trans = ColumnTransformer(
+                transformers=[
+                    (name, pipe["transformer"], pipe["columns"])
+                    for name, pipe in team_pipe_meta.items()
+                ],
+            )
+            transformer_list.append(("team", team_trans))
 
-        processor_pipe = FeatureUnion(
-            transformer_list=[
-                ("player", player_trans),
-                ("ball", ball_trans),
-                ("timer", timer_trans),
-                ("booster", booster_trans),
-                ("team", team_trans),
-            ],
-        )
+        if len(transformer_list) > 0:
+            processor_pipe = FeatureUnion(
+                transformer_list=transformer_list,
+            )
+        else:
+            processor_pipe = "passthrough"
 
         self.base_pipeline = Pipeline(
             steps=[
